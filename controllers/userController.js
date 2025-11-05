090108567f8f1f87d6f2130d8e6ef2765e2aa6fe
ileshthesiya <114352983+ileshthesiya@users.noreply.github.com>
2025-11-05 21:24:39 +0530

    Update userController.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,8 +1,6 @@
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const User = require("../models/user");
-const SubscriptionPlan = require("../models/subscriptionPlan");
-const UserSubscription = require("../models/userSubscription");
 
 // Register a new user
 exports.register = async (req, res) => {
@@ -112,115 +110,5 @@ exports.userDetails = async (req, res) => {
     return res
       .status(500)
       .send({ message: "Something went to wong in get all user" });
-  }
-};
-
-// User subscribes to a plan
-exports.subscribeToPlan = async (req, res) => {
-  try {
-    const { userId, planId } = req.body;
-    const { loginUser } = req;
-    if (!userId || !planId) {
-      return res.status(400).send({ message: "All fields are required." });
-    }
-
-    // if (loginUser?._id != userId || loginUser?.role !== "user") {
-    //   return res.status(401).send({ message: "Unauthorized access." });
-    // }
-
-    await UserSubscription.deleteMany({
-      userId: userId,
-    });
-
-    const plan = await SubscriptionPlan.findById(planId);
-
-    let currentDate = new Date();
-    let expirationDate = new Date(
-      currentDate.setMonth(currentDate.getMonth() + 1)
-    );
-
-    // Add 5 hours and 30 minutes
-    expirationDate.setHours(expirationDate.getHours() + 5);
-    expirationDate.setMinutes(expirationDate.getMinutes() + 30);
-
-    const userSubscription = new UserSubscription({
-      userId,
-      planId,
-      features: [...plan.features], // Snapshot the features
-      validUntil: expirationDate, // Set expiration to exactly 1 month from today
-    });
-
-    await userSubscription.save();
-    res
-      .status(200)
-      .json({ message: "Subscription successful", userSubscription });
-  } catch (err) {
-    console.log("Somthing went to wrong into subscribe to plan.", err);
-    res.status(500).json({ error: "Subscription failed" });
-  }
-};
-
-// Get user's subscription features
-exports.getUserFeatures = async (req, res) => {
-  try {
-    const { userId } = req.body;
-    const { loginUser } = req;
-    if (!userId) {
-      return res.status(400).send({ message: "All fields are required." });
-    }
-
-    if (loginUser?._id != userId) {
-      return res.status(401).send({ message: "Unauthorized access." });
-    }
-    const userSubscription = await UserSubscription.findOne({ userId: userId });
-    if (!userSubscription) {
-      return res.status(404).json({ message: "Empty set" });
-    }
-
-    const planData = await SubscriptionPlan.findOne({
-      _id: userSubscription.planId,
-    });
-
-    const features = Array.from(
-      new Set([...userSubscription.features, ...planData.features])
-    );
-
-    const result = {
-      _id: planData?._id,
-      name: planData?.name,
-      description: planData?.description,
-      price: planData?.price,
-      features: features,
-      subscriptionDate: userSubscription?.subscriptionDate,
-      validUntil: userSubscription?.validUntil,
-    };
-
-    res.status(200).json({ data: result });
-  } catch (err) {
-    console.log("Something went to wrong into get user feature.", err);
-    res.status(500).json({ error: "Error retrieving features" });
-  }
-};
-
-exports.getSingleSubscription = async (req, res) => {
-  try {
-    const { planId, userId } = req.body;
-    const { loginUser } = req;
-    if (!userId || !planId) {
-      return res.status(400).send({ message: "All fields are required." });
-    }
-
-    if (loginUser?._id != userId) {
-      return res.status(401).send({ message: "Unauthorized access." });
-    }
-
-    const planData = await SubscriptionPlan.findOne({
-      _id: planId,
-    });
-
-    res.status(200).json({ data: planData });
-  } catch (err) {
-    console.log("Something went to wrong into get user feature.", err);
-    res.status(500).json({ error: "Error retrieving features" });
   }
 };

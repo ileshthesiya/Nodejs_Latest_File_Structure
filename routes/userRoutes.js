9d37ce8c4a0956eaa57179761b3cfc8818c0d1f3
ileshthesiya <114352983+ileshthesiya@users.noreply.github.com>
2025-11-06 09:20:44 +0530

    Update userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,7 +3,6 @@ const router = express.Router();
 const userController = require("../controllers/userController");
 const { jwtValidation } = require("../middleware/auth");
 
-// User routes for subscribing to a plan and viewing features
 router.post("/register", userController.register);
 router.post("/login", userController.login);
 router.post("/userDetails", jwtValidation, userController.userDetails);

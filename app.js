0cbe267a19a7b83315b1cec5bb9ba8481b34e2a4
ileshthesiya <114352983+ileshthesiya@users.noreply.github.com>
2025-11-06 09:22:14 +0530

    Update app.js

--- app.js
+++ app.js
@@ -4,25 +4,19 @@ const bodyParser = require("body-parser");
 require("dotenv").config();
 const cors = require("cors");
 
-// Import routes
-const userRoutes = require("./routes/userRoutes");
-
 const app = express();
 
-// Middleware
 app.use(bodyParser.json());
 app.use(cors());
 
-// Routes
+const userRoutes = require("./routes/userRoutes");
 app.use("/user", userRoutes);
 
-// Database Connection
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB Connected"))
   .catch((err) => console.log("Database connection error:", err));
 
-// Start the server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
